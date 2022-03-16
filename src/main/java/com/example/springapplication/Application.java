cb6d8c2b33c0e77546c4b720d06d34b68644498d
visl <5vladik068@gmail.com>
2022-03-16 20:44:11 +0200

    ToDo: Errors Tests

--- src/main/java/com/example/springapplication/Application.java
+++ src/main/java/com/example/springapplication/Application.java
@@ -2,7 +2,11 @@ package com.example.springapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories("repository")
+@EntityScan("controller")
 @SpringBootApplication
 public class Application {
 
---
+++ src/main/java/entity/Book.java
@@ -0,0 +1,31 @@
+package entity;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(nullable = false, unique = true)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+
+    public long getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
